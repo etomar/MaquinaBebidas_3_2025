a9558feff2a852cfd0e7f7a243a9f3e2092b1be6
Esther Diaz <ed.alum.daw@gmail.com>
2025-04-10 10:39:50 +0200

    todo junto

--- src/maquinabebidas_3_2025/MaquinaDeBebidas.java
+++ src/maquinabebidas_3_2025/MaquinaDeBebidas.java
@@ -1,8 +1,10 @@
+package maquinabebidas_3_2025;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package maquinabebidas_3_2025;
+
 
 /**
  * Clase Maquina de bebidas
